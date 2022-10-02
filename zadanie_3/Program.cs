f673f746afa79db760a2e6fa5d8252c5a9a704d4
Marat <brutto061@gmail.com>
2022-10-02 21:47:10 +0700

    Выполнили третье задание

---
+++ zadanie_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+int b = int.Parse (Console.ReadLine() ?? "0");
+Console.WriteLine("Введите третье число");
+int c = int.Parse (Console.ReadLine() ?? "0");
+
+if(a > b) 
+{
+  if(a > c) Console.WriteLine("Максимальное число " + a);
+  else Console.WriteLine("Максимальное число " + c);
+}
+else
+{
+  if(b > c) Console.WriteLine("Максимальное число " + b);
+  else Console.WriteLine("Максимальное число " + c);
+}
