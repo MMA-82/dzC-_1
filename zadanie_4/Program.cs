dbaf795aa242b6cc0e0a4fb8ba3b42d494521275
Marat <brutto061@gmail.com>
2022-10-03 21:51:21 +0700

    Выполнили четвертое задание

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Ведите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if(N % 2 == 0) Console.WriteLine("Введенное число четное");
+else Console.WriteLine("Введенное число нечетное");
