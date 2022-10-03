ba262974c8b04a5d2c3f298ecd3220f747d74973
Marat <brutto061@gmail.com>
2022-10-03 23:02:32 +0700

    Выполнили задание №6

---
+++ zadanie_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число, до которого искать все четные");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int x = 1;
+while(x <= N)
+{
+    if(x % 2 == 0) Console.WriteLine( + x);
+    x = x + 1;
+}
