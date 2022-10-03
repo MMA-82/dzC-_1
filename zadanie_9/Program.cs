d08d27a1c1d5064d1428f8fda7bdb7944c2b5c5a
Marat <brutto061@gmail.com>
2022-10-03 23:51:40 +0700

    Выполнили задание №9

---
+++ zadanie_9/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число от 10 до 99");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int a = N / 10;
+int b = N % 10;
+if(a > b) Console.WriteLine("Наибольшая цифра у введенного числа: " + a);
+else Console.WriteLine("Наибольшая цифра у введенного числа: " + b);
