4a523d5a052d46d83ec1e3b7face97f5a7e54091
Marat <brutto061@gmail.com>
2022-10-04 22:26:24 +0700

    Выполнили задание №11

---
+++ zadanie_11/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Ведите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if(N % 5 == 0) Console.WriteLine("Введенное число кратно 5");
+else Console.WriteLine("Введенное число не кратно 5");
