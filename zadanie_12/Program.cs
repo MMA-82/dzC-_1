1acebb73694db6d606352236490bd795f670d9ab
Marat <brutto061@gmail.com>
2022-10-04 23:13:57 +0700

    Выполнили задание №12

---
+++ zadanie_12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if(N > 99)
+{
+    if(N < 1000)
+    {
+    int x = N % 10;
+    Console.WriteLine("Третья цифра введенного числа " + x);
+    }
+    else Console.WriteLine("Введенное число имеет более трех цифр");
+}
+else Console.WriteLine("У введенного числа нет третьей цифры!");
