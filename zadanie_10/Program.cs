02a84218de5056e8f9ddd0ba4a7bc88c659b0719
Marat <brutto061@gmail.com>
2022-10-04 22:03:24 +0700

    Выполнили задание №10

---
+++ zadanie_10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int result = (N/100)*10 + N%10;
+Console.WriteLine("Убрали среднюю цифру числа: " + result);
