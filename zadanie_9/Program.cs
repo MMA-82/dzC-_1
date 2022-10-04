a69e97689bb76090466dfd40b882d0e627afcf4a
Marat <brutto061@gmail.com>
2022-10-04 21:47:08 +0700

    Поправили задание №9

--- zadanie_9/Program.cs
+++ zadanie_9/Program.cs
@@ -1,6 +1,14 @@
-﻿Console.WriteLine("Введите число от 10 до 99");
+﻿Console.WriteLine("Введите любое двузначное число");
 int N = int.Parse(Console.ReadLine() ?? "0");
-int a = N / 10;
-int b = N % 10;
-if(a > b) Console.WriteLine("Наибольшая цифра у введенного числа: " + a);
-else Console.WriteLine("Наибольшая цифра у введенного числа: " + b);
+if(N <= 99)
+{
+    if(N >= 10)
+    {
+        int a = N / 10;
+        int b = N % 10;
+        if(a > b) Console.WriteLine("Наибольшая цифра у введенного числа: " + a);
+        else Console.WriteLine("Наибольшая цифра у введенного числа: " + b);
+    }
+    else Console.WriteLine("Введено не двузначное число!!!");    
+}
+else Console.WriteLine("Введено не двузначное число!!!"); 
